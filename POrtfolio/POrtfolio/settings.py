3fc39c4460d7733aa90a48fc84eb316baa0e38ca
shreytrivedi002 <shreytrivedi002@gmail.com>
2021-07-21 16:43:19 +0530

    basic changes

--- POrtfolio/POrtfolio/settings.py
+++ POrtfolio/POrtfolio/settings.py
@@ -129,3 +129,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
