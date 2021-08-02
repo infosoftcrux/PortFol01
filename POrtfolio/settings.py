8cc2ae6d8907fb5ed1666366b916f1adfbeda70c
shreytrivedi002 <shreytrivedi002@gmail.com>
2021-08-02 18:22:48 +0530

    change

--- POrtfolio/settings.py
+++ POrtfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Email initilise
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
